bfdb00538e9788ce2efa59c109b56881c0b97196
Fabricio Liquesley <109369219+fabricioliquesley@users.noreply.github.com>
2023-10-13 19:43:43 -0300

    Upload files
    uploading todo list files

---
+++ todolist/src/test/java/br/com/fabricioliquesley/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabricioliquesley.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
